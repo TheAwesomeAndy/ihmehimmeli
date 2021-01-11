f307c953edd3a0154ce9f44082eec90b86dc8648
TheAwesomeAndy <68536805+TheAwesomeAndy@users.noreply.github.com>
2021-01-11 00:31:59 -0500

    changed links in download_mnist to fashionMNIST

--- download_mnist.sh
+++ download_mnist.sh
@@ -15,9 +15,9 @@
 
 mkdir -p data/mnist
 pushd data/mnist
-wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+wget https://github.com/zalandoresearch/fashion-mnist/blob/master/data/fashion/t10k-images-idx3-ubyte.gz
+wget https://github.com/zalandoresearch/fashion-mnist/blob/master/data/fashion/t10k-labels-idx1-ubyte.gz
+wget https://github.com/zalandoresearch/fashion-mnist/blob/master/data/fashion/train-images-idx3-ubyte.gz
+wget https://github.com/zalandoresearch/fashion-mnist/blob/master/data/fashion/train-labels-idx1-ubyte.gz
 gzip -d *.gz
 popd
